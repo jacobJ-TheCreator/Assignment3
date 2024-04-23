c57e54cddc008e13be89293e195d1e6d980bfa46
Jacob Jobse <166144656+jacobJ-TheCreator@users.noreply.github.com>
2024-04-22 23:52:21 -0600

    Add files via upload

---
+++ scripts/contact.js
@@ -0,0 +1,16 @@
+// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+
+// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+
+submitButton.addEventListner('click', function(event){
+    event.preventDefault();
+
+    let message = document.createElement('p');
+    message.textContent = 'Thank you fr your message!';
+    message.style.fontSize ='24px';
+    message.style.marginTop = '20px';
+    message.style.marginLeft = '100px';
+
+    contactPage.innerHTML = '';
+    contactPage.appendChild(message);
+});
